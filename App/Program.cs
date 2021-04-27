f23f5281ebff9712bfb0e97989e7e5e17fb47d1b
ShevchukDm <shevchuk_d@donnu.edu.ua>
2021-04-28 00:55:16 +0300

    First commmit

---
+++ App/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace App
+{
+    class Program
+    {
+
+        static bool isEven(int inputNumber)
+        {
+            return inputNumber % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isEven(2));
+            Console.WriteLine(isEven(3));
+            Console.ReadKey();
+        }
+    }
+}
